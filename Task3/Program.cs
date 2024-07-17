3203ff36ffac64354ee4d77eaad5ebffdccd29a3
Igor <igor-igor-73@mail.ru>
2024-07-17 23:05:38 +0500

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+
+void Main()
+{
+    int[] array = { 1, 2, 5, 0, 10, 34 };
+    ReversArr(array, array.Length - 1);
+}
+void ReversArr(int[] array, int i)
+{
+    if (i < 0) return;
+    Console.Write(array[i] + " ");
+    ReversArr(array, i - 1);
+}
+Main();
